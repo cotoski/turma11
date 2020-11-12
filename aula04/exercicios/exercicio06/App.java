ebfb74084e6cd3c69e7e7ef254f3fed048b07a4f
EmersonPaduan <emerson.paduan@gmail.com>
2020-11-12 18:00:48 -0300

    aula04-resposta exerc extra-robo

---
+++ aula04/exercicios/exercicio06/App.java
@@ -0,0 +1,19 @@
+package exercicios.exercicio06;
+
+public class App {
+    public static void main(String[] args) {
+        Robo robo = new Robo(3, 3);
+
+        System.out.println(robo.posicaoAtual());
+        robo.moverOeste();
+        System.out.println(robo.posicaoAtual());
+        robo.moverOeste();
+        System.out.println(robo.posicaoAtual());
+        robo.moverOeste();
+        System.out.println(robo.posicaoAtual());
+        robo.moverOeste();
+        System.out.println(robo.posicaoAtual());
+        robo.moverOeste();
+        System.out.println(robo.posicaoAtual());
+    }
+}
