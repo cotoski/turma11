3f4bd0df247edb6cd6ffcf1fa7cd07a9ec915fa7
EmersonPaduan <emerson.paduan@gmail.com>
2020-11-18 17:56:31 -0300

    aula08 final

--- aula08/exercicio03/AppConta.java
+++ aula08/exercicio03/AppConta.java
@@ -1,17 +1,97 @@
 package exercicio03;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AppConta {
     public static void main(String[] args) {
-        ContaCorrente cc = new ContaCorrente(1111);
-        ContaEspecial ce = new ContaEspecial(22222, 100);
+        Scanner teclado = new Scanner(System.in);
+        int menu;
+        int numeroConta;
+        double limite, valor;
+        ArrayList<Conta> listaContas = new ArrayList<>();
+        Conta conta = null;
 
-        cc.depositar(100);
-        cc.sacar(500);
-        cc.sacar(50);
-        System.out.println(cc);
+        do {
+            System.out.println("1-Nova Conta Corrente");
+            System.out.println("2-Nova Conta Especial");
+            System.out.println("3-Nova Conta Poupança");
+            System.out.println("4-Fazer Depósito");
+            System.out.println("5-fazer saque");
+            System.out.println("6-Exibir saldo");
+            System.out.println("7-Sair");
+            menu = teclado.nextInt();
 
-        ce.depositar(200);
-        ce.sacar(250);
-        System.out.println(ce);
+            switch (menu) {
+                case 1:
+                    System.out.println("Informe o número da conta:");
+                    numeroConta = teclado.nextInt();
+                    conta = new ContaCorrente(numeroConta);
+                    listaContas.add(conta);
+                    break;
+
+                case 2:
+                    System.out.println("Informe o número da conta:");
+                    numeroConta = teclado.nextInt();
+                    System.out.println("Informe o valor do limite:");
+                    limite = teclado.nextDouble();
+                    conta = new ContaEspecial(numeroConta, limite);
+                    listaContas.add(conta);
+                    break;
+
+                case 3:
+                    System.out.println("Informe o número da conta:");
+                    numeroConta = teclado.nextInt();
+                    conta = new ContaPoupanca(numeroConta);
+                    listaContas.add(conta);
+                    break;
+
+                case 4:
+                    System.out.println("Informe o número da conta:");
+                    numeroConta = teclado.nextInt();
+                    System.out.println("Informe o valor do depósito:");
+                    valor = teclado.nextDouble();
+                    for (Conta c : listaContas) {
+                        if (c.getNumero() == numeroConta) {
+                            c.depositar(valor);
+                        }
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Informe o número da conta:");
+                    numeroConta = teclado.nextInt();
+                    System.out.println("Informe o valor do saque:");
+                    valor = teclado.nextDouble();
+                    for (Conta c : listaContas) {
+                        if (c.getNumero() == numeroConta) {
+                            c.sacar(valor);
+                        }
+                    }
+                    break;
+
+                case 6:
+                    System.out.println("Informe o número da conta:");
+                    numeroConta = teclado.nextInt();
+                    for (Conta c : listaContas) {
+                        if (c.getNumero() == numeroConta) {
+                            System.out.println(c);
+                        }
+                    }
+                    break;
+
+                case 7:
+
+                    break;
+
+                default: // se não for nenhuma das opções acima
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+
+        } while (menu != 7);
+
+        teclado.close();
     }
+
 }
---
+++ aula08/exercicio03/ContaPoupanca.java
@@ -0,0 +1,23 @@
+package exercicio03;
+
+public class ContaPoupanca extends Conta{
+    private static double taxaSaque = 0.1;
+
+    public ContaPoupanca(int numeroConta){
+        super(numeroConta);
+    }
+
+    public static void setTaxaSaque(double taxa){
+        if(taxa > 0){
+            taxaSaque = taxa;
+        }
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if(valor > 0 && valor <= getSaldo() + taxaSaque ){
+            super.sacar(valor + taxaSaque);
+        }
+    }
+
+}
